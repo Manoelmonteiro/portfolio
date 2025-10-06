520d75f80d5359047cec6b75a7d7ac29df1d4932
Manoel <63215619+Manoelmonteiro@users.noreply.github.com>
2025-10-06 19:19:57 -0300

    adicionei links, transicoes nos links, animacao de fogo

--- fogo.js
+++ fogo.js
@@ -1,32 +1,39 @@
+// Pega o canvas da página
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const W = canvas.width = 1920;
-const H = canvas.height = 1080;
+
+// Define largura e altura
+const W = canvas.width = window.innerWidth;
+const H = canvas.height = window.innerHeight;
+
+// OffscreenCanvas
 const ofc = new OffscreenCanvas(W, H);
 const octx = ofc.getContext("2d");
+
 function frame(t) {
   octx.reset();
   octx.fillStyle = '#000';
   octx.fillRect(0, 0, W, H);
+
   for (let i = 0; i < 2000; ++i) {
     octx.fillStyle = `rgba(255, ${255 - i / 8}, ${255 - i})`;
     octx.fillRect(
       960 + Math.sin(i / 8) * i / 2,
       1030 - i * Math.tan(i ** 4 + t / 1000),
-      30, 50);
+      30, 50
+    );
   }
+
   requestAnimationFrame(frame);
+
   ctx.reset();
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, W, H);
-  ctx.filter = new CanvasFilter([
-    {filter: "gaussianBlur", stdDeviation: 24},
-    {filter:"colorMatrix", values: [
-      4, 0, 0, 0, -1.5,
-      0, 4, 0, 0, -1.5,
-      0, 0, 4, 0, -1.5,
-      0, 0, 0, 1, 0,
-    ]},
-  ]);
+
+  // filtro nativo (blur básico)
+  ctx.filter = "blur(8px)";
+
   ctx.drawImage(ofc, 0, 0);
 }
+
 frame(0);
